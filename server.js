923ab1f3f4cbab3a46809b47d2ded7390713b73f
chanakaudithasandaruwan <121252932+webshealth@users.noreply.github.com>
2023-01-31 00:30:14 +0530

    2nd day

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 const http = require('http');
 const express = require('express');
 const dotenv = require('dotenv');
+const logger = require('./middleware/logger');
+const morgan = require('morgan');
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
+const users = [
+  { fName: 'a', age: 26 },
+  { fName: 'b', age: 53 },
+  { fName: 'c', age: 26 },
+  { fName: 'D', age: 50 },
+];
+users.reduce((acc, curr) => {
+  if (curr.age === acc.age) {
+    users[0].age;
+    // acc={users[0].age:1+1}
+  } else {
+    // acc.[`${curr.age}`]=1
+  }
+}, 0);
 
 //load env vars
-
 dotenv.config({ path: './config/config.env' });
+
 const app = express();
+
+//Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(logger);
+
+// Mount routers
+
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
